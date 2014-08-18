adfa726ed177fd44467675354557ae7c2bfc2dd3
epicallan <lukwagoallan@yahoo.com>
2014-08-18 20:08:00 +0300

    removed files

--- wp-content/themes/starkers-master/cinema/allan_php/allan_disable_cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-header("Expires: 0");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
-?>
--- wp-content/themes/starkers-master/cinema/allan_php/allan_verify_money.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require'allan_verify_purchase.php';
-	if(isset($_POST['verify'])){
-	$verify= new verify();
-	$verify->read_mobile_mValues();
-	
-	}
-?>
